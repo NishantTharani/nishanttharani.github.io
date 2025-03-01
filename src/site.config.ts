b417c3befb684e3216b5cff42c07194f0438a9f8
Nishant Tharani <nishant.tharani.work@gmail.com>
2025-02-28 16:16:00 -0800

    Add url

--- src/site.config.ts
+++ src/site.config.ts
@@ -22,8 +22,7 @@ export const siteConfig: SiteConfig = {
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
 	title: "Nish",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	// url: "https://www.ntharani.com",
-	url: "https://nishanttharani.github.io",
+	url: "https://ntharani.com",
 };
 
 // Used to generate links in both the Header & Footer.
